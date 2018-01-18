ecfa1bcf27cefcc022500f6b77d1575719cda808
Chase Bowden and Kayla Ondracek <kayla@>
2018-01-18 14:32:09 -0800

    add handling for capital vowels

--- js/scripts.js
+++ js/scripts.js
@@ -34,25 +34,25 @@ function singleWordTranslate(text){
     return text + " is a number. We need a word, my friend.";
   }
   //tests to see if user input is a vowel and if length is 1 then applies rule
-  if (text.length === 1 && /[aeiou]/.test(text)){
+  if (text.length === 1 && /[aeiou]/i.test(text)){
     return (text + "ay");
   }
   //tests to see if user input length is >1 and is a vowel and applies rule
-  if (/[aeiou]/.test(text[0]) && text.length > 1) {
+  if (/[aeiou]/i.test(text[0]) && text.length > 1) {
     return(text + "way");
   }
   //tests to see if a word starts with qu
-  if (/[^aeiou]/.test(text[0]) && /[^aeio]/.test(text[1]) && /[^0-9]/.test(text[0])) {
+  if (/[^aeiou]/i.test(text[0]) && /[^aeio]/i.test(text[1]) && /[^0-9]/.test(text[0])) {
     for (var i = 0; /[^aeioy]/.test(text[i]); i++) {
     }
     return(text.slice(i, ) + text.slice(0, i) + "ay");
   }
   //Tests to see if word begins with single consonant and applies rule
-  if (/[^aeiou]/.test(text[0]) && /[aeiou]/.test(text[1])){
+  if (/[^aeiou]/i.test(text[0]) && /[aeiou]/i.test(text[1])){
     return(text.slice(1, ) + text[0]+ "ay");
   }
   //tests to see if a word starts with multiple consonants
-  if (/[^aeiou]/.test(text[0]) && /[^aeiou]/.test(text[1]) && /[^0-9]/.test(text[0])) {
+  if (/[^aeiou]/i.test(text[0]) && /[^aeiou]/i.test(text[1]) && /[^0-9]/.test(text[0])) {
     for (var i = 0; /[^aeiou]/.test(text[i]); i++) {
     }
     console.log(i);
